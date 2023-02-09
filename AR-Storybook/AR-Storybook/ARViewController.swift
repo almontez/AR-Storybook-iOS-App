0bfe669d4c3524354d4646ded2b6efdeab39b3f9
Angela Montez <angelali.montez@gmail.com>
2023-02-09 11:05:59 -0800

    Place models on pages 11-21

--- AR-Storybook/AR-Storybook/ARViewController.swift
+++ AR-Storybook/AR-Storybook/ARViewController.swift
@@ -115,10 +115,123 @@ class ARViewController: UIViewController, ARSCNViewDelegate {
                         planeNode.addChildNode(pigNode)
                     }
                 }
+                
+            case "page11":
+                let modelScene = SCNScene(named: "art.scnassets/Models/mesh_pg11.scn")!
+                
+                let eleNode = modelScene.rootNode.childNode(withName: "Gerald_11", recursively: false)!
+                eleNode.eulerAngles.x = .pi
+                planeNode.addChildNode(eleNode)
+                
+                let pigNode = modelScene.rootNode.childNode(withName: "Piggie_11", recursively: false)!
+                pigNode.eulerAngles.x = .pi
+                planeNode.addChildNode(pigNode)
+                
+            case "page12":
+                let modelScene = SCNScene(named: "art.scnassets/Models/mesh_pg12.scn")!
+                
+                let eleNode = modelScene.rootNode.childNode(withName: "Gerald_12", recursively: false)!
+                eleNode.eulerAngles.x = .pi
+                planeNode.addChildNode(eleNode)
+                
+                let pigNode = modelScene.rootNode.childNode(withName: "Piggie_12", recursively: false)!
+                pigNode.eulerAngles.x = .pi
+                planeNode.addChildNode(pigNode)
+            
+            case "page13":
+                let modelScene = SCNScene(named: "art.scnassets/Models/mesh_pg13.scn")!
+                
+                let eleNode = modelScene.rootNode.childNode(withName: "Gerald_13", recursively: false)!
+                eleNode.eulerAngles.x = .pi
+                planeNode.addChildNode(eleNode)
+                
+                let pigNode = modelScene.rootNode.childNode(withName: "Piggie_13", recursively: false)!
+                pigNode.eulerAngles.x = .pi
+                planeNode.addChildNode(pigNode)
+
+            case "page14":
+                let modelScene = SCNScene(named: "art.scnassets/Models/mesh_pg14.scn")!
+                
+                let eleNode = modelScene.rootNode.childNode(withName: "Gerald_14", recursively: false)!
+                eleNode.eulerAngles.x = .pi
+                planeNode.addChildNode(eleNode)
+                
+                let pigNode = modelScene.rootNode.childNode(withName: "Piggie_14", recursively: false)!
+                pigNode.eulerAngles.x = .pi
+                planeNode.addChildNode(pigNode)
+            
+            case "page15":
+                let modelScene = SCNScene(named: "art.scnassets/Models/mesh_pg15.scn")!
+                
+                let eleNode = modelScene.rootNode.childNode(withName: "Gerald_15", recursively: false)!
+                eleNode.eulerAngles.x = .pi
+                planeNode.addChildNode(eleNode)
+                
+                let pigNode = modelScene.rootNode.childNode(withName: "Piggie_15", recursively: false)!
+                pigNode.eulerAngles.x = .pi
+                planeNode.addChildNode(pigNode)
+            
+            case "page16":
+                let modelScene = SCNScene(named: "art.scnassets/Models/mesh_pg16.scn")!
+                
+                let eleNode = modelScene.rootNode.childNode(withName: "Gerald_16", recursively: false)!
+                eleNode.eulerAngles.x = .pi
+                planeNode.addChildNode(eleNode)
+                
+                let pigNode = modelScene.rootNode.childNode(withName: "Piggie_16", recursively: false)!
+                pigNode.eulerAngles.x = .pi
+                planeNode.addChildNode(pigNode)
+                
+            case "page17":
+                let modelScene = SCNScene(named: "art.scnassets/Models/mesh_pg17.scn")!
+                
+                let eleNode = modelScene.rootNode.childNode(withName: "Gerald_17", recursively: false)!
+                eleNode.eulerAngles.x = .pi
+                planeNode.addChildNode(eleNode)
+                
+                let pigNode = modelScene.rootNode.childNode(withName: "Piggie_17", recursively: false)!
+                pigNode.eulerAngles.x = .pi
+                planeNode.addChildNode(pigNode)
+            
+            case "page18":
+                let modelScene = SCNScene(named: "art.scnassets/Models/mesh_pg18.scn")!
+                
+                let eleNode = modelScene.rootNode.childNode(withName: "Gerald_18", recursively: false)!
+                eleNode.eulerAngles.x = .pi
+                planeNode.addChildNode(eleNode)
+            
+            case "page19":
+                let modelScene = SCNScene(named: "art.scnassets/Models/mesh_pg19.scn")!
+                
+                let pigNode = modelScene.rootNode.childNode(withName: "Piggie_19", recursively: false)!
+                pigNode.eulerAngles.x = .pi
+                planeNode.addChildNode(pigNode)
+            
+            case "page20":
+                let modelScene = SCNScene(named: "art.scnassets/Models/mesh_pg20.scn")!
+                
+                let eleNode = modelScene.rootNode.childNode(withName: "Gerald_20", recursively: false)!
+                eleNode.eulerAngles.x = .pi
+                planeNode.addChildNode(eleNode)
+                
+                let pigNode = modelScene.rootNode.childNode(withName: "Piggie_20", recursively: false)!
+                pigNode.eulerAngles.x = .pi
+                planeNode.addChildNode(pigNode)
+            
+            case "page21":
+                let modelScene = SCNScene(named: "art.scnassets/Models/mesh_pg21.scn")!
+                
+                let eleNode = modelScene.rootNode.childNode(withName: "Gerald_21", recursively: false)!
+                eleNode.eulerAngles.x = .pi
+                planeNode.addChildNode(eleNode)
+                
+                let pigNode = modelScene.rootNode.childNode(withName: "Piggie_21", recursively: false)!
+                pigNode.eulerAngles.x = .pi
+                planeNode.addChildNode(pigNode)
+                
             default:
                 print("Page not found")
             }
-            
         }
         
         return node
