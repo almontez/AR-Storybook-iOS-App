a78458671b086219d92d9fc415f037bd0d2791ac
Garrick Chan <changarrickt@gmail.com>
2023-02-10 15:47:07 -0800

    Added recognition behavior for pages 4-10, implemented follow-camera for certain pages

--- AR-Storybook/AR-Storybook/ARViewController.swift
+++ AR-Storybook/AR-Storybook/ARViewController.swift
@@ -16,8 +16,12 @@ class ARViewController: UIViewController, ARSCNViewDelegate {
     @IBOutlet var soundButton: UIButton!
     
     // Information of the current page
-    var currentAnchor : ARAnchor!
+    var currentAnchor : ARImageAnchor!
     var currentNode : SCNNode!
+    // Original rotation of the current node
+    var currentRotationOffset : Float = 0.0
+    // Whether to constrain the model to the camera
+    var lookAtCamera = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -64,6 +68,25 @@ class ARViewController: UIViewController, ARSCNViewDelegate {
 
     // MARK: - ARSCNViewDelegate
     
+    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+        if lookAtCamera && currentNode != nil{
+            currentNode.eulerAngles.y = -sceneView.session.currentFrame!.camera.eulerAngles.y + currentRotationOffset
+        }
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        print("--Nodes--")
+        for n in sceneView.scene.rootNode.childNodes{
+            if n.name != nil{
+                print(n.name!)
+            }
+        }
+        print("---------")
+        if lookAtCamera{
+            currentRotationOffset = currentNode.eulerAngles.y
+        }
+    }
+    
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
         let node = SCNNode()
         node.eulerAngles.x = .pi/2.0
@@ -71,9 +94,9 @@ class ARViewController: UIViewController, ARSCNViewDelegate {
         // Is the anchor an image?
         if let imageAnchor = anchor as? ARImageAnchor{
             
+            clearPage()
             switch imageAnchor.referenceImage.name{
             case "BookCover":
-                clearCurrent()
                 let modelScene = SCNScene(named: "art.scnassets/Models/mesh_cover.scn")!
 
                 let eleNode = modelScene.rootNode.childNode(withName: "Gerald_01", recursively: false)!
@@ -85,12 +108,8 @@ class ARViewController: UIViewController, ARSCNViewDelegate {
                 pigNode.worldPosition = SCNVector3(0.05, 0.0, 0.08)
                 node.addChildNode(pigNode)
 
-                node.name = imageAnchor.referenceImage.name
-                currentNode = node
-                currentAnchor = anchor
-                print("Cover detected")
+                updatePage(node: node, imageAnchor: imageAnchor)
             case "page2":
-                clearCurrent()
                 let modelScene = SCNScene(named: "art.scnassets/Models/mesh_pg02.scn")!
 
                 let eleNode = modelScene.rootNode.childNode(withName: "Gerald_02", recursively: false)!
@@ -98,13 +117,10 @@ class ARViewController: UIViewController, ARSCNViewDelegate {
 
                 let pigNode = modelScene.rootNode.childNode(withName: "Piggie_02", recursively: false)!
                 node.addChildNode(pigNode)
-
-                node.name = imageAnchor.referenceImage.name
-                currentNode = node
-                currentAnchor = anchor
-                print("Page 2 detected")
+                
+                lookAtCamera = true
+                updatePage(node: node, imageAnchor: imageAnchor)
             case "page3":
-                clearCurrent()
                 let modelScene = SCNScene(named: "art.scnassets/Models/mesh_pg03.scn")!
 
                 let eleNode = modelScene.rootNode.childNode(withName: "Gerald_03", recursively: false)!
@@ -113,10 +129,78 @@ class ARViewController: UIViewController, ARSCNViewDelegate {
                 let pigNode = modelScene.rootNode.childNode(withName: "Piggie_03", recursively: false)!
                 node.addChildNode(pigNode)
 
-                node.name = imageAnchor.referenceImage.name
-                currentNode = node
-                currentAnchor = anchor
-                print("Page 3 detected")
+                lookAtCamera = true
+                updatePage(node: node, imageAnchor: imageAnchor)
+            case "page4":
+                let modelScene = SCNScene(named: "art.scnassets/Models/mesh_pg04.scn")!
+
+                let eleNode = modelScene.rootNode.childNode(withName: "Gerald_04", recursively: false)!
+                node.addChildNode(eleNode)
+
+                let pigNode = modelScene.rootNode.childNode(withName: "Piggie_04", recursively: false)!
+                node.addChildNode(pigNode)
+
+                updatePage(node: node, imageAnchor: imageAnchor)
+            case "page5":
+                let modelScene = SCNScene(named: "art.scnassets/Models/mesh_pg05.scn")!
+
+                let eleNode = modelScene.rootNode.childNode(withName: "Gerald_05", recursively: false)!
+                node.addChildNode(eleNode)
+
+                let pigNode = modelScene.rootNode.childNode(withName: "Piggie_05", recursively: false)!
+                node.addChildNode(pigNode)
+
+                updatePage(node: node, imageAnchor: imageAnchor)
+            case "page6":
+                let modelScene = SCNScene(named: "art.scnassets/Models/mesh_pg06.scn")!
+
+                let eleNode = modelScene.rootNode.childNode(withName: "Gerald_06", recursively: false)!
+                node.addChildNode(eleNode)
+
+                let pigNode = modelScene.rootNode.childNode(withName: "Piggie_06", recursively: false)!
+                node.addChildNode(pigNode)
+
+                updatePage(node: node, imageAnchor: imageAnchor)
+            case "page7":
+                let modelScene = SCNScene(named: "art.scnassets/Models/mesh_pg07.scn")!
+
+                let eleNode = modelScene.rootNode.childNode(withName: "Gerald_07", recursively: false)!
+                node.addChildNode(eleNode)
+
+                let pigNode = modelScene.rootNode.childNode(withName: "Piggie_07", recursively: false)!
+                node.addChildNode(pigNode)
+
+                updatePage(node: node, imageAnchor: imageAnchor)
+            case "page8":
+                let modelScene = SCNScene(named: "art.scnassets/Models/mesh_pg08.scn")!
+
+                let eleNode = modelScene.rootNode.childNode(withName: "Gerald_08", recursively: false)!
+                node.addChildNode(eleNode)
+
+                let pigNode = modelScene.rootNode.childNode(withName: "Piggie_08", recursively: false)!
+                node.addChildNode(pigNode)
+
+                updatePage(node: node, imageAnchor: imageAnchor)
+            case "page9":
+                let modelScene = SCNScene(named: "art.scnassets/Models/mesh_pg09.scn")!
+
+                let eleNode = modelScene.rootNode.childNode(withName: "Gerald_09", recursively: false)!
+                node.addChildNode(eleNode)
+
+                let pigNode = modelScene.rootNode.childNode(withName: "Piggie_09", recursively: false)!
+                node.addChildNode(pigNode)
+
+                updatePage(node: node, imageAnchor: imageAnchor)
+            case "page10":
+                let modelScene = SCNScene(named: "art.scnassets/Models/mesh_pg10.scn")!
+
+                let eleNode = modelScene.rootNode.childNode(withName: "Gerald_10", recursively: false)!
+                node.addChildNode(eleNode)
+
+                let pigNode = modelScene.rootNode.childNode(withName: "Piggie_10", recursively: false)!
+                node.addChildNode(pigNode)
+
+                updatePage(node: node, imageAnchor: imageAnchor)
             case "page11":
                 let modelScene = SCNScene(named: "art.scnassets/Models/mesh_pg11.scn")!
                 
@@ -126,6 +210,7 @@ class ARViewController: UIViewController, ARSCNViewDelegate {
                 let pigNode = modelScene.rootNode.childNode(withName: "Piggie_11", recursively: false)!
                 node.addChildNode(pigNode)
                 
+                updatePage(node: node, imageAnchor: imageAnchor)
             case "page12":
                 let modelScene = SCNScene(named: "art.scnassets/Models/mesh_pg12.scn")!
                 
@@ -135,6 +220,7 @@ class ARViewController: UIViewController, ARSCNViewDelegate {
                 let pigNode = modelScene.rootNode.childNode(withName: "Piggie_12", recursively: false)!
                 node.addChildNode(pigNode)
             
+                updatePage(node: node, imageAnchor: imageAnchor)
             case "page13":
                 let modelScene = SCNScene(named: "art.scnassets/Models/mesh_pg13.scn")!
                 
@@ -144,6 +230,7 @@ class ARViewController: UIViewController, ARSCNViewDelegate {
                 let pigNode = modelScene.rootNode.childNode(withName: "Piggie_13", recursively: false)!
                 node.addChildNode(pigNode)
 
+                updatePage(node: node, imageAnchor: imageAnchor)
             case "page14":
                 let modelScene = SCNScene(named: "art.scnassets/Models/mesh_pg14.scn")!
                 
@@ -153,6 +240,7 @@ class ARViewController: UIViewController, ARSCNViewDelegate {
                 let pigNode = modelScene.rootNode.childNode(withName: "Piggie_14", recursively: false)!
                 node.addChildNode(pigNode)
             
+                updatePage(node: node, imageAnchor: imageAnchor)
             case "page15":
                 let modelScene = SCNScene(named: "art.scnassets/Models/mesh_pg15.scn")!
                 
@@ -162,6 +250,7 @@ class ARViewController: UIViewController, ARSCNViewDelegate {
                 let pigNode = modelScene.rootNode.childNode(withName: "Piggie_15", recursively: false)!
                 node.addChildNode(pigNode)
             
+                updatePage(node: node, imageAnchor: imageAnchor)
             case "page16":
                 let modelScene = SCNScene(named: "art.scnassets/Models/mesh_pg16.scn")!
                 
@@ -171,6 +260,7 @@ class ARViewController: UIViewController, ARSCNViewDelegate {
                 let pigNode = modelScene.rootNode.childNode(withName: "Piggie_16", recursively: false)!
                 node.addChildNode(pigNode)
                 
+                updatePage(node: node, imageAnchor: imageAnchor)
             case "page17":
                 let modelScene = SCNScene(named: "art.scnassets/Models/mesh_pg17.scn")!
                 
@@ -180,18 +270,21 @@ class ARViewController: UIViewController, ARSCNViewDelegate {
                 let pigNode = modelScene.rootNode.childNode(withName: "Piggie_17", recursively: false)!
                 node.addChildNode(pigNode)
             
+                updatePage(node: node, imageAnchor: imageAnchor)
             case "page18":
                 let modelScene = SCNScene(named: "art.scnassets/Models/mesh_pg18.scn")!
                 
                 let eleNode = modelScene.rootNode.childNode(withName: "Gerald_18", recursively: false)!
                 node.addChildNode(eleNode)
             
+                updatePage(node: node, imageAnchor: imageAnchor)
             case "page19":
                 let modelScene = SCNScene(named: "art.scnassets/Models/mesh_pg19.scn")!
                 
                 let pigNode = modelScene.rootNode.childNode(withName: "Piggie_19", recursively: false)!
                 node.addChildNode(pigNode)
             
+                updatePage(node: node, imageAnchor: imageAnchor)
             case "page20":
                 let modelScene = SCNScene(named: "art.scnassets/Models/mesh_pg20.scn")!
                 
@@ -201,6 +294,7 @@ class ARViewController: UIViewController, ARSCNViewDelegate {
                 let pigNode = modelScene.rootNode.childNode(withName: "Piggie_20", recursively: false)!
                 node.addChildNode(pigNode)
             
+                updatePage(node: node, imageAnchor: imageAnchor)
             case "page21":
                 let modelScene = SCNScene(named: "art.scnassets/Models/mesh_pg21.scn")!
                 
@@ -210,6 +304,7 @@ class ARViewController: UIViewController, ARSCNViewDelegate {
                 let pigNode = modelScene.rootNode.childNode(withName: "Piggie_21", recursively: false)!
                 node.addChildNode(pigNode)
                 
+                updatePage(node: node, imageAnchor: imageAnchor)
             default:
                 print("Page not found")
             }
@@ -218,10 +313,25 @@ class ARViewController: UIViewController, ARSCNViewDelegate {
         return node
     }
     
-    func clearCurrent(){
+    func clearPage(){
+        // Removes the current anchor so it can be added again later
         if currentAnchor != nil{
             sceneView.session.remove(anchor: currentAnchor)
         }
+        // Removes the previous node from the scene
+        if currentNode != nil{
+            currentNode.removeFromParentNode()
+        }
+        // Reset look-at variables
+        lookAtCamera = false
+        currentRotationOffset = 0.0
+    }
+    
+    func updatePage(node: SCNNode, imageAnchor: ARImageAnchor){
+        node.name = imageAnchor.referenceImage.name
+        currentNode = node
+        currentAnchor = imageAnchor
+        print("\(node.name!) detected")
     }
     
     // MARK: - UI Functionalities
