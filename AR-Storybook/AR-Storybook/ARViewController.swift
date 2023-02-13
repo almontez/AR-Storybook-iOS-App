8f6269e3856dcf995ad13a797c997414a3ca5119
Angela Montez <angelali.montez@gmail.com>
2023-02-12 22:09:39 -0800

    Implement Button Color Change

--- AR-Storybook/AR-Storybook/ARViewController.swift
+++ AR-Storybook/AR-Storybook/ARViewController.swift
@@ -239,7 +239,15 @@ class ARViewController: UIViewController, ARSCNViewDelegate {
     
     // MARK: - UI Functionalities
     
+    // Changes color of button when pressed down - UI feature ONLY
+    @IBAction func soundButtonPressedDown(_ sender: UIButton) {
+        soundButton.backgroundColor = UIColor(red: 138/255, green: 255/255, blue: 138/255, alpha: 1.0)
+    }
+    
+    // Play audio for tutorial
     @IBAction func soundButtonPressed(_ sender: UIButton) {
         print("AR View Sound Button Pressed")
+        // change color of button back to gray when released
+        soundButton.backgroundColor = .gray
     }
 }
--- AR-Storybook/AR-Storybook/GuideViewController.swift
+++ AR-Storybook/AR-Storybook/GuideViewController.swift
@@ -20,8 +20,16 @@ class GuideViewController: UIViewController {
         
     }
     
+    // Changes color of button when pressed down - UI feature ONLY
+    @IBAction func soundButtonPressedDown(_ sender: UIButton) {
+        soundButton.backgroundColor = UIColor(red: 138/255, green: 255/255, blue: 138/255, alpha: 1.0)
+    }
+    
+    // Play audio for tutorial
     @IBAction func soundButtonPressed(_ sender: UIButton) {
         print("Guide View Sound Button Pressed")
+        // change color of button back to gray when released
+        soundButton.backgroundColor = .gray
     }
         
 }
